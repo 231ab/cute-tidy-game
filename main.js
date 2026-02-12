f5ce17bd34abbbae7605c0042f6cc2a9da30ecfc
231ab <160825206+231ab@users.noreply.github.com>
2026-02-12 10:24:24 +0800

    Initialize Phaser game with Boot, Menu, and Game scenes
    Removed old JSON configuration and added Phaser game setup.

---
+++ main.js
@@ -0,0 +1,18 @@
+import { BootScene } from './scenes/BootScene.js';
+import { MenuScene } from './scenes/MenuScene.js';
+import { GameScene } from './scenes/GameScene.js';
+
+const config = {
+    type: Phaser.AUTO,
+    parent: "game-container",
+    width: 390,
+    height: 844,
+    backgroundColor: "#FFF0F5",
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    scene: [BootScene, MenuScene, GameScene]
+};
+
+new Phaser.Game(config);
